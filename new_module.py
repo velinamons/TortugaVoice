4b0f352f08b03959791c3963064bda6dcfdfa782
velinamons <velinamons@gmail.com>
2024-04-21 20:03:36 +0300

    add property decorators(getter, setter), clear() function create

--- new_module.py
+++ new_module.py
@@ -1,18 +1,26 @@
 from __future__ import annotations
 
+
 class Genius:
     def __init__(self, memory: int | None = None):
         self._memory = memory
 
-    def get_memory(self) -> int | None:
+    @property
+    def memory(self) -> int | None:
         return self._memory
 
-    def set_memory(self, number: int) -> None:
+    @memory.setter
+    def memory(self, number: int) -> None:
         self._memory = number
+
+    def clear(self) -> None:
+        self._memory = 0
+
 
 if __name__ == "__main__":
     new_genius = Genius(5)
-    print(new_genius.get_memory())
-    print(new_genius.set_memory(5))
-
-
+    print(new_genius.memory)
+    new_genius.memory = 5
+    print(new_genius.memory)
+    new_genius.clear()
+    print(new_genius.memory)
